cae61ae26d1e590081cb85d596271741a1613892
John Chenier <johnchenier@Johns-iMac.local>
2018-12-13 13:46:25 -0500

    initial commit

---
+++ js/Diap.js
@@ -0,0 +1,7 @@
+var intElemScrollTop = someElement.scrollTop;var intElemScrollTop = someElement.scrollTop;$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    // Do something
+});$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    // Do something
+});
